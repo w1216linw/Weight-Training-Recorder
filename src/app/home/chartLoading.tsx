902a30df4801cf5bd6117d9dd78f42c98ba87f03
weiwei <w1216lin@gmail.com>
2024-12-13 15:29:26 -0600

    :construction:

--- src/app/home/chartLoading.tsx
+++ src/app/home/chartLoading.tsx
@@ -1,5 +1,5 @@
 const LoadingChart = () => {
-  return <div className="skeleton aspect-square w-full opacity-50"></div>;
+  return <div className="skeleton aspect-square w-full opacity-50 p-2"></div>;
 };
 
 export default LoadingChart;
